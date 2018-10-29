736776fa74291b26c16b48698e97a72f8ac377be
jewooSong <iamsjy0808@gmail.com>
2018-10-30 03:30:16 +0900

    라우트 적용

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { Demo, Main, NotFoundPage } from 'pages';
 
-const App = () => <div>Planet</div>;
+const App = () => (
+  <div>
+    <Switch>
+      <Route exact path="/" component={Main} />
+      <Route exact path="/Demo" component={Demo} />
+      <Route exact component={NotFoundPage} />
+    </Switch>
+  </div>
+);
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as Demo } from './Demo';
+export { default as Main } from './Main';
+export { default as NotFoundPage } from './NotFoundPage';
