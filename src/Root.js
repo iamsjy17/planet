9516b50ca635826b3ccdff05be0975909f51f570
jewooSong <iamsjy0808@gmail.com>
2018-11-03 04:16:52 +0900

    history 적용, github page에서 라우트 사용

--- src/Root.js
+++ src/Root.js
@@ -3,12 +3,17 @@ import { BrowserRouter } from 'react-router-dom';
 import App from 'components/App';
 import { Provider } from 'react-redux';
 import configure from 'store/configure';
+import createHistory from 'history/createBrowserHistory';
 
-const store = configure();
+const history = createHistory({
+  basename: process.env.PUBLIC_URL,
+});
+
+const store = configure({ history });
 
 const Root = () => (
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter history={history}>
       <App />
     </BrowserRouter>
   </Provider>
