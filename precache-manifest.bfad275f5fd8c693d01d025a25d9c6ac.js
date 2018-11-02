82d313e979a0888db0887e532c37ad08cdcb98a7
jewooSong <iamsjy0808@gmail.com>
2018-11-03 03:36:47 +0900

    Updates

---
+++ precache-manifest.bfad275f5fd8c693d01d025a25d9c6ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da4cc84e91b029864115",
+    "url": "/planet.git/static/js/runtime~main.da4cc84e.js"
+  },
+  {
+    "revision": "9239ccf97fe8a66ac78d",
+    "url": "/planet.git/static/js/main.9239ccf9.chunk.js"
+  },
+  {
+    "revision": "7ef34a6b8ec3c92e138b",
+    "url": "/planet.git/static/js/1.7ef34a6b.chunk.js"
+  },
+  {
+    "revision": "9239ccf97fe8a66ac78d",
+    "url": "/planet.git/static/css/main.b248cc13.chunk.css"
+  },
+  {
+    "revision": "b33d5eef0def658aec17a86039f06b75",
+    "url": "/planet.git/index.html"
+  }
+];
